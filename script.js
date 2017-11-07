bdb0437ac1ff5a3be2c2c641f7cd9eed0ea08aba
Peter Gelsomino <petergelsomino@Peters-MBP.home>
2017-11-06 21:06:19 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+/*Script.JS*/
+
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+// Highlight the top nav as scrolling occurs
+ $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+});
+
+// Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function (){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Floating label headings for the contact form
+    $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+    $('.coachImg').hover(function() {
+    $(this).fadeTo("fast" , 0.7);
+		}, function() {
+    $(this).fadeTo("fast" , 1);
+  });   
+    
+    
+    $( "#formSubmit" ).click(function() {
+        //Test click function was working
+        //alert( "Handler for .click() called." );
+
+              var name = $('#name').val();
+              var age = $('#age').val();
+              var phone = $('#phone').val();
+              var email = $('#email').val();               
+              var primPos = $('#primaryPos').val();
+              var secPos = $('#secPos').val();
+              var prevTravel = $('#prevTravel').val();
+              var message = $('#message').val();
+          
+          var playerInfo = "<br> Name: "+ name+"<br>"           
+          + "<br> Age: "+ age+"<br>"
+          + "<br> Phone: "+ phone+"<br>"
+          + "<br> Email: "+ email+"<br>"
+          + "<br> Primary Position: "+ primPos+"<br>"
+          + "<br> Secondary Position: "+ secPos+"<br>"
+          + "<br> Previous Travel Team: "+ prevTravel+"<br>"
+          + "<br> Message: "+ message+"<br>";
+          
+        // For Testing
+       // console.log(playerInfo);
+  if ((name!=="") && (age!=="") && (phone!=="") && (email!=="") && (primPos!=="") && (secPos!=="") && (prevTravel!=="") && (message!=="")) {   
+  emailjs.send("default_service","akadema",{name: "Test", playerInfo: playerInfo})
+            .then(function(response) {
+                console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+                swal("Success!", "You will hear from our coaches shortly", "success")
+            }, function(err) {
+                console.log("FAILED. error=", err);
+            swal("Something Went Wrong...", "Please Try Again!", "error")
+            });
+        } else {
+            swal("Please fill out all fields", "Please Try Again!", "error")
+        }
+    }); 
+    
+//     swal({    
+//                confirmButtonColor: "#d9534f",   
+//        }); 
+  
+  })(jQuery); // End of use strict
---
+++ sweetAlertJS.js
@@ -0,0 +1,14 @@
+<!--Script for Alerts-->
+<script>
+      $(window).load(function(){
+        swal({    
+                title: "WINTER WORKOUTS!",   
+                text: "Email coach Dan Padrazo, winter workouts are going on now!  akademaprospects@gmail.com",
+                type: "warning",   
+                showCancelButton: false,   
+                confirmButtonColor: "#d9534f",   
+                confirmButtonText: "OK, Got it!",   
+                closeOnConfirm: false
+        }); 
+      });
+</script>   
